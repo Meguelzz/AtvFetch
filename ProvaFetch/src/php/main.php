e46485886ff5fd5d10f2709e6a13b9694d3b703f
Meguelzz <meguelzz.oliveira@gmail.com>
2025-05-19 20:02:26 -0300

    Update main.php

--- ProvaFetch/src/php/main.php
+++ ProvaFetch/src/php/main.php
@@ -1,22 +1,22 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+    <?php
+    session_start(); 
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $cidade = file_get_contents('php://input');
-    $_SESSION['historico'][] = [
-        'cidade' => $cidade,
-        'data' => date('d/m/Y H:i')
-    ];
-    echo json_encode($_SESSION['historico']);
-    exit;
-}
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $cidade = file_get_contents('php://input');
+        $_SESSION['historico'][] = [
+            'cidade' => $cidade,
+            'data' => date('d/m/Y H:i')
+        ];
+        echo json_encode($_SESSION['historico']);
+        exit;
+    }
 
-if (isset($_GET['limpar'])) {
-    unset($_SESSION['historico']);
-    echo json_encode([]);
-    exit;
-}
+    if (isset($_GET['limpar'])) {
+        unset($_SESSION['historico']);
+        echo json_encode([]);
+        exit;
+    }
 
-echo json_encode($_SESSION['historico'] ?? []);
+    echo json_encode($_SESSION['historico'] ?? []);
